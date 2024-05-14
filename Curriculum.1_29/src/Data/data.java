b4b77eebe99c6f880690180ab317ced83d387b67
user.email <choit@LAPTOP-RVEN208A>
2024-05-15 00:08:05 +0900

    課題1.29の訂正

--- Curriculum.1_29/src/Data/data.java
+++ Curriculum.1_29/src/Data/data.java
@@ -8,24 +8,33 @@ public class data implements Comparable<data> {
 	private String 県庁所在地;
 	private double 面積;
 
+	//コンストラクタ作成
 	public data(String 都道府県名, String 県庁所在地, double 面積) {
+	//フィールドnoにクラス変数の値seqを代入し、値を1ずつ増加させる。
 	no = seq++;
+	//コンストラクタの引数として渡された都道府県名をクラスのフィールドの都道府県名に代入する。
 	this.都道府県名 = 都道府県名;
+	//以下二行も上の行と同様。
 	this.県庁所在地 = 県庁所在地;
 	this.面積 = 面積;
 	}
 
+	//noフィールドの値を返し、並び変えるための番号を取得する。
 	public int getNo() {
 	return no;
 	}
 
 	@Override
+	//返り値がString型のtoStringメソッドを作する。
 	public String toString() {
+	//String.formatを使用して都道府県名・県庁所在地・面積のフィールドを文字列として返す。
 	return String.format("都道府県名：%s%n県庁所在地：%s%n面積：%.1fkm2%n", 都道府県名, 県庁所在地, 面積);
 	}
 	
 	@Override
+	//data型のオブジェクトを引数にとり、整数を返すcompareToメソッドを作成する。
 	public int compareTo(data o) {
+	//thisとnoに引数として渡されたoを比較する。
 	return this.getNo() - o.getNo();
 	}
 	
--- Curriculum.1_29/src/Main/main.java
+++ Curriculum.1_29/src/Main/main.java
@@ -8,8 +8,10 @@ import java.util.stream.Stream;
 import Data.data;
 
 public class main {
+	//コンソールに入力された値を受け取る
 	private static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
+		//コンソールに出力するリストdataを作成
 		List<data> list = List.of(new data("北海道", "札幌市", 83424),
 				 new data("青森県", "青森市", 9646),
 				 new data("岩手県", "盛岡市", 15275),
@@ -22,9 +24,13 @@ public class main {
 				 new data("群馬県", "前橋市", 6362),
 				 new data("埼玉県", "さいたま市", 3798));
 		
+		//コンソールで受け取った値を空白で区切る
 		String[] array = scanner.nextLine().split(" ");
+		//配列arrayにあるカンマ区切りの数値文字列を分割してintに変換し、リストnoListを作成する。
 		List<Integer> noList = Stream.of(array[0].split(",")).mapToInt(Integer::parseInt).boxed().toList();
+		//配列array[1]の値が降順の文字列に等しいかチェック。trueは降順、falseは昇順に並び変える。
 		Comparator<data> order = "降順".equals(array[1]) ? Comparator.reverseOrder() : Comparator.naturalOrder();
+		//各要素pのgetNoメソッドの結果がリストnoListにある要素だけを残して、上の行で生成したorderを並び変えて出力する。
 		list.stream().filter(p -> noList.contains(p.getNo())).sorted(order).forEach(System.out::println);
 	}
 }
