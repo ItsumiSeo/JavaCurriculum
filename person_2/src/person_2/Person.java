a3dab2b82f98b414fd8d28fed712f7bcf9659b04
user.email <choit@LAPTOP-RVEN208A>
2024-05-18 22:10:54 +0900

    課題1.31の提出

--- person_2/src/person_2/Person.java
+++ person_2/src/person_2/Person.java
@@ -1,40 +1,24 @@
 package person_2;
 
 class Person {
-	// クラスフィールドを定義
+	// 問1クラスフィールドを定義
 	public static int count = 0;
 
 	// インスタンスフィールドを定義
-	public String firstName;
-	public String lastName;
+	public String name;
 	public int age;
 	public double height;
 	public double weight;
 
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String firstName, int age, double height, double weight) {
+	Person(String name, int age, double height, double weight) {
+		//問2クラスフィールドcountに１足す
 		Person.count++;
 
-		this.firstName = firstName;
+		this.name = name;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
-	}
-
-	// コンストラクタのオーバーロード
-	Person(String firstName, String lastName, int age, double height, double weight) {
-		Person.count++;
-
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.height = height;
-		this.weight = weight;
-	}
-
-	// インスタンスメソッドfullNameを定義
-	public String fullName() {
-		return this.firstName + this.lastName;
 	}
 
 	//インスタンスメソッドbmiを定義
@@ -45,11 +29,13 @@ class Person {
 
 	//インスタンスメソッドprintを定義
 	public void print() {
-		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("名前は" + this.name + "です");
 		System.out.println("年は" + this.age + "です");
 	}
 
+	//問4クラスメソッド「printCount」を定義
 	public static void printCount() {
+		//問5
 		System.out.println("合計" + Person.count + "人です");
 	}
 
